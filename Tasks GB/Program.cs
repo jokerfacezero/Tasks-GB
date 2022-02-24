824f6b238a9be92c2b618b3a51278149e92f4dcf
jokerfacezer <mitrander36@gmail.com>
2022-02-25 01:12:20 +0600

    For Pull Request

---
+++ Tasks GB/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Name?");
+
+string name;
+
+while(true)
+{
+    name = Console.ReadLine();
+    Console.WriteLine(name);
+    Console.WriteLine(DateTime.Now.ToString());
+    Console.ReadLine();
+    if(name == "lll")
+    {
+        break;
+
+    }
+    else
+    {
+        Console.Clear();
+    }
+}
