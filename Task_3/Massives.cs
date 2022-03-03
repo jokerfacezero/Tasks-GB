8565c438379b0530b6d308639d9f5c99e2060d41
ILDUS SALIKHOV <95938324+jokerfacezero@users.noreply.github.com>
2022-03-04 00:32:50 +0600

    Non master (#4)
    Added 3 task

---
+++ Task_3/Massives.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class Massives
+    {
+        int[,] arr = new int[2, 2];
+        
+        public void GetMassive()
+        {
+            var rand = new Random();
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = rand.Next(100);
+                }
+            }
+        }
+        public void PrintMassive()
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 1; j < arr.GetLength(1); j++)
+                {
+                    Console.Write($"{arr[i, i]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Task_3/ReverseString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class ReverseString
+    {
+        string hello = "hello";
+        public void ReverseWords() { 
+        Console.WriteLine(hello);
+            for (int i = hello.Length; i > 0; i--)
+            {
+                char nowChar = hello[i - 1];
+                Console.Write(nowChar);
+            }
+        }
+
+        publ
+}
+}
